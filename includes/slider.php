dfa2a984d0f56086cd76b5131a50352d2e0d92d1
Samet Topal <astopaal@gmail.com>
2023-04-03 03:54:05 +0300

    add slider

---
+++ includes/slider.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="content-type" content="text/html;charset=utf-8"/>
+    <title>Amazing Slider</title>
+    
+    <!-- Insert to your webpage before the </head> -->
+    <script src="assets/sliderengine/jquery.js"></script>
+    <script src="assets/sliderengine/amazingslider.js"></script>
+    <link rel="stylesheet" type="text/css" href="assets/sliderengine/amazingslider-1.css">
+    <script src="assets/sliderengine/initslider-1.js"></script>
+    <link rel="stylesheet" href="assets/styles/style.css">
+    <!-- End of head section HTML codes -->
+    
+</head>
+<body>
+
+    <div class="amazingslider-wrapper" id="amazingslider-wrapper-1" style="display:block;position:relative;max-width:900px;margin:0px auto 56px;">
+        <div class="amazingslider" id="amazingslider-1" style="display:block;position:relative;margin:0 auto;">
+            <ul class="amazingslider-slides" style="display:none;">
+                <li><img src="assets/images/46-900x360.jpg" alt="Lorem ipsum"  title="Lorem ipsum" data-description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." />
+                </li>
+                <li><img src="assets/images/136-900x360.jpg" alt="Lorem ipsum"  title="Lorem ipsum" data-description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum" />
+                </li>
+                <li><img src="assets/images/564-900x360.jpg" alt="Lorem ipsum"  title="Lorem ipsum" data-description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum" />
+                </li>
+                <li><img src="assets/images/837-900x360.jpg" alt="Lorem ipsum"  title="Lorem ipsum" data-description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum" />
+                </li>
+            </ul>
+            <ul class="amazingslider-thumbnails" style="display:none;">
+                <li><img src="assets/images/46-900x360-tn.jpg" alt="Lorem ipsum" title="Lorem ipsum" /></li>
+                <li><img src="assets/images/136-900x360-tn.jpg" alt="Lorem ipsum" title="Lorem ipsum" /></li>
+                <li><img src="assets/images/564-900x360-tn.jpg" alt="Lorem ipsum" title="Lorem ipsum" /></li>
+                <li><img src="assets/images/837-900x360-tn.jpg" alt="Lorem ipsum" title="Lorem ipsum" /></li>
+            </ul>
+        </div>
+    </div>
+    <!-- End of body section HTML codes -->
+    
+</body>
+</html>
