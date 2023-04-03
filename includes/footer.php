8297881858f07fb65b4b8057e8d87b20908cf7b8
cihan <cihand0_43@hotmail.com>
2023-04-03 17:36:47 +0300

    footer tamam

--- includes/footer.php
+++ includes/footer.php
@@ -5,32 +5,53 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Footer</title>
-    <link rel="stylesheet" href="assets/style-foot.css">
+    <link rel="stylesheet" href="../assets/styles/style-foot.css">
 </head>
 <body>
 <footer class="footer">
     <header class="head-contact">
-        <h1>Contact</h1>
+        
     </header>
-    <div class="comment">
-        <label for="name">First Name</label> <input id="name" type="text">
-        <label for="surname">Last Name</label> <input id="surname" type="text"> <br>
-        <label for="email">Email *</label> <input id="email" type="text" required>
-        <label for="">Message</label> <textarea name="" id="comment" cols="30" rows="10"></textarea>
-    </div>
-
-    <div class="contact">
-        <img src="gassets/icons/location.png">
+    
+<div class="contact">
+    <div class="footer-left">
+        <img src="../assets/icons/location.png">
         <p>Prof Dr Yaşar UÇAR caddesi Zabıta binası kat:2 no:204 Serik/Antalya/TÜRKİYE</p>
-        <img src="gassets/icons/mail.png">
+        <img src="../assets/icons/mail.png">
         <p>erasmusmobilityturkey@gmail.com</p>
-        <div class="websites">
-            <a href="index.html"><img src="assets/icons/facebook.png" alt="fb-photo"></a>
-            <a href="index.html"><img src="assets/icons/twitter.png" alt="twitter-photo"></a>
-            <a href="index.html"><img src="assets/icons/linkedin.png" alt="linkedin-photo"></a>
-            <a href="index.html"><img src="assets/icons/instagram.png" alt="instagram-photo"></a>
+            <div class="websites">
+              
+                  <a href="https://www.facebook.com/username/"><img src="../assets/icons/facebook.png" alt="fb-photo"></a>
+                  <a href="https://twitter.com/username"><img src="../assets/icons/twitter.png" alt="twitter-photo"></a>
+                  <a href="https://www.linkedin.com/in/username/"><img src="../assets/icons/linkedin.png" alt="linkedin-photo"></a>
+                  <a href="https://www.instagram.com/username/"><img src="../assets/icons/instagram.png" alt="instagram-photo"></a>
+             </div>    
+      </div>
+
+        <div class="map">
+          <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d195.43823568902997!2d27.075564800179652!3d38.39497688318127!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1str!2str!4v1680528380910!5m2!1str!2str" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
-    </div>
+              <div class="footer-right">
+                <h1>Contact</h1>
+                <form>
+                  <div class="name-field">
+                  <div class="name">
+                    <label for="name">First Name</label>
+                    <input id="name" type="text">
+                  </div>
+                  <div class="name">
+                    <label for="surname">Last Name</label> 
+                    <input id="surname" type="text"> 
+                  </div>
+                </div>
+                    <label for="email">Email *</label>
+                    <input id="email" type="email" required>
+                    <label for="comment">Message</label>
+                    <textarea name="" id="comment" ></textarea>
+                    <input type="submit" value="Gönder">
+                </form>
+              </div>
+  </div>            
     <div class="copyright">
         <p>copyright©2022, erasmusmobilityturkey.com</p>
     </div>
