83393f011124ecc5b58e037afd9ca2e9b3d367e5
Samet Topal <astopaal@gmail.com>
2023-04-03 14:37:58 +0300

    add events field and cards. set header position to fixed.

--- includes/slider.php
+++ includes/slider.php
@@ -1,16 +1,25 @@
-<!DOCTYPE html>
-<html>
 <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
     <style>
-        * {box-sizing: border-box}
-        body {font-family: Verdana, sans-serif; margin:0}
-        .mySlides {display: none}
-        img {vertical-align: middle;}
+        * {
+            box-sizing: border-box
+        }
+
+        .body {
+            font-family: Verdana, sans-serif;
+            margin: 0
+        }
+
+        .mySlides {
+            display: none
+        }
+
+        img {
+            vertical-align: middle;
+        }
 
         /* Slideshow container */
         .slideshow-container {
-            margin-top : 30px !important;
+            margin-top: 30px !important;
             max-width: 1400px;
             position: relative;
             margin: auto;
@@ -40,7 +49,7 @@
 
         /* On hover, add a black background color with a little bit see-through */
         .prev:hover, .next:hover {
-            background-color: rgba(0,0,0,0.8);
+            background-color: rgba(0, 0, 0, 0.8);
         }
 
         /* Caption text */
@@ -86,17 +95,23 @@
         }
 
         @keyframes fade {
-            from {opacity: .4}
-            to {opacity: 1}
+            from {
+                opacity: .4
+            }
+            to {
+                opacity: 1
+            }
         }
 
         /* On smaller screens, decrease text size */
         @media only screen and (max-width: 300px) {
-            .prev, .next,.text {font-size: 11px}
+            .prev, .next, .text {
+                font-size: 11px
+            }
         }
     </style>
 </head>
-<body>
+<body class="body">
 
 <div class="slideshow-container">
 
@@ -134,13 +149,13 @@
     let slideIndex = 1;
     showSlides(slideIndex);
 
-    let slideTimer = setInterval(function() {
+    let slideTimer = setInterval(function () {
         plusSlides(1);
     }, 5000); // slaytın 5 saniyede bir değişmesi için
 
     function plusSlides(n) {
         clearInterval(slideTimer); // yeni slaytı elle değiştirdiğinizde, otomatik geçişi durdurun
-        slideTimer = setInterval(function() {
+        slideTimer = setInterval(function () {
             plusSlides(1);
         }, 5000); // yeniden başlatın
         showSlides(slideIndex += n);
@@ -148,7 +163,7 @@
 
     function currentSlide(n) {
         clearInterval(slideTimer); // yeni slaytı elle değiştirdiğinizde, otomatik geçişi durdurun
-        slideTimer = setInterval(function() {
+        slideTimer = setInterval(function () {
             plusSlides(1);
         }, 5000); // yeniden başlatın
         showSlides(slideIndex = n);
@@ -158,18 +173,22 @@
         let i;
         let slides = document.getElementsByClassName("mySlides");
         let dots = document.getElementsByClassName("dot");
-        if (n > slides.length) {slideIndex = 1}
-        if (n < 1) {slideIndex = slides.length}
+        if (n > slides.length) {
+            slideIndex = 1
+        }
+        if (n < 1) {
+            slideIndex = slides.length
+        }
         for (i = 0; i < slides.length; i++) {
             slides[i].style.display = "none";
         }
         for (i = 0; i < dots.length; i++) {
             dots[i].className = dots[i].className.replace(" active", "");
         }
-        slides[slideIndex-1].style.display = "block";
-        dots[slideIndex-1].className += " active";
+        slides[slideIndex - 1].style.display = "block";
+        dots[slideIndex - 1].className += " active";
     }
 </script>
 
 </body>
-</html>
+
